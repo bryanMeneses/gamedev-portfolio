7a4c0ae62aaca496da09607c1cf0929364793932
Bryan Meneses <brianmeneses@Brians-MacBook-Pro.local>
2020-06-06 18:09:07 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,43 @@
+// Elements 
+var loadingScreen = document.getElementById('loadingScreen');
+var portfolioWrapper = document.getElementById('portfolioWrapper');
+var iconsDiv = document.getElementById('icons');
+
+// miliseconds delay
+var miliseconds = 2500;
+
+addGhosts(4, miliseconds);
+
+// Add ghosts sequentially on loading screen
+function addGhosts(length, ms) {
+  var icons = [
+    'fas fa-gamepad',
+    'fas fa-ghost',
+    'fas fa-headset',
+    'fas fa-dice-d6',
+    'fas fa-dice-d20',
+    'far fa-heart',
+    'fas fa-gavel',
+    'fas fa-hat-wizard',
+    'fas fa-dragon',
+    'fas fa-scroll'
+  ];
+
+  var rngIcon = icons[Math.floor(Math.random() * icons.length)];
+
+  for (var i = 0; i < length; i++) {
+    var delay = i * (ms / length);
+
+    setTimeout(function() {
+      var icon = document.createElement('i');
+      icon.setAttribute('class', 'icon ' + rngIcon);
+      iconsDiv.appendChild(icon);
+    }, delay);
+  }
+}
+
+// Remove loading screen after 2 seconds
+setTimeout(() => {
+  loadingScreen.classList.add('hidden');
+  portfolioWrapper.classList.remove('hidden');
+}, miliseconds);
